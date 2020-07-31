fa568654edec49b02fbd41c350c9317db1d835a6
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2020-07-31 15:07:09 +0530

    lib: data: Use lower resolution image for category tiles
    Change-Id: I4517335e1d5377eaec3314e06a116dadd14b0a51

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -9,21 +9,21 @@ List<CategoriesModel> getCategories() {
 
   //
   CategoryModel.imgUrl =
-      "https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+      "https://images.pexels.com/photos/2116475/pexels-photo-2116475.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100";
   CategoryModel.CategoryName = "Bikes";
   categories.add(CategoryModel);
   CategoryModel = new CategoriesModel();
 
   //
   CategoryModel.imgUrl =
-      "https://images.pexels.com/photos/1149137/pexels-photo-1149137.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+      "https://images.pexels.com/photos/1149137/pexels-photo-1149137.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100";
   CategoryModel.CategoryName = "Cars";
   categories.add(CategoryModel);
   CategoryModel = new CategoriesModel();
 
   //
   CategoryModel.imgUrl =
-      "https://images.pexels.com/photos/466685/pexels-photo-466685.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+      "https://images.pexels.com/photos/466685/pexels-photo-466685.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100";
   CategoryModel.CategoryName = "City";
   categories.add(CategoryModel);
   CategoryModel = new CategoriesModel();
@@ -37,21 +37,21 @@ List<CategoriesModel> getCategories() {
 
   //
   CategoryModel.imgUrl =
-      "https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+      "https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=100";
   CategoryModel.CategoryName = "Nature";
   categories.add(CategoryModel);
   CategoryModel = new CategoriesModel();
 
   //
   CategoryModel.imgUrl =
-      "https://images.pexels.com/photos/545008/pexels-photo-545008.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+      "https://images.pexels.com/photos/545008/pexels-photo-545008.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100";
   CategoryModel.CategoryName = "Street Art";
   categories.add(CategoryModel);
   CategoryModel = new CategoriesModel();
 
   //
   CategoryModel.imgUrl =
-      "https://images.pexels.com/photos/704320/pexels-photo-704320.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
+      "https://images.pexels.com/photos/704320/pexels-photo-704320.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=100";
   CategoryModel.CategoryName = "Wild Life";
   categories.add(CategoryModel);
   CategoryModel = new CategoriesModel();
